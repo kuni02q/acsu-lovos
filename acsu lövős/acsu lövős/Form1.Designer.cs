e2a2d31f4ed64202bb4651cb4b8531253a2a910f
kuni02q <70653128+kuni02q@users.noreply.github.com>
2020-09-02 18:38:19 +0200

    Add files via upload

---
+++ acsu lövős/acsu lövős/Form1.Designer.cs
@@ -0,0 +1,170 @@
+﻿namespace acsu_lövős
+{
+    partial class kepernyo
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(kepernyo));
+            this.acsu = new System.Windows.Forms.PictureBox();
+            this.szoveg1 = new System.Windows.Forms.Label();
+            this.szoveg2 = new System.Windows.Forms.Label();
+            this.szoveg3 = new System.Windows.Forms.Label();
+            this.kilep = new System.Windows.Forms.Button();
+            this.ujrakezd = new System.Windows.Forms.Button();
+            this.idozito = new System.Windows.Forms.Timer(this.components);
+            this.szoveg4 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.acsu)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // acsu
+            // 
+            this.acsu.BackColor = System.Drawing.Color.Transparent;
+            this.acsu.Image = ((System.Drawing.Image)(resources.GetObject("acsu.Image")));
+            this.acsu.Location = new System.Drawing.Point(191, 314);
+            this.acsu.Name = "acsu";
+            this.acsu.Size = new System.Drawing.Size(95, 114);
+            this.acsu.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.acsu.TabIndex = 0;
+            this.acsu.TabStop = false;
+            this.acsu.Click += new System.EventHandler(this.acsu_Click);
+            // 
+            // szoveg1
+            // 
+            this.szoveg1.AutoSize = true;
+            this.szoveg1.BackColor = System.Drawing.Color.Transparent;
+            this.szoveg1.Font = new System.Drawing.Font("Microsoft Sans Serif", 22.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.szoveg1.Location = new System.Drawing.Point(12, 9);
+            this.szoveg1.Name = "szoveg1";
+            this.szoveg1.Size = new System.Drawing.Size(109, 44);
+            this.szoveg1.TabIndex = 1;
+            this.szoveg1.Text = "Pont:";
+            // 
+            // szoveg2
+            // 
+            this.szoveg2.AutoSize = true;
+            this.szoveg2.BackColor = System.Drawing.Color.Transparent;
+            this.szoveg2.Font = new System.Drawing.Font("Microsoft Sans Serif", 22.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.szoveg2.Location = new System.Drawing.Point(12, 62);
+            this.szoveg2.Name = "szoveg2";
+            this.szoveg2.Size = new System.Drawing.Size(199, 44);
+            this.szoveg2.TabIndex = 2;
+            this.szoveg2.Text = "Szar vagy:";
+            // 
+            // szoveg3
+            // 
+            this.szoveg3.AutoSize = true;
+            this.szoveg3.BackColor = System.Drawing.Color.Transparent;
+            this.szoveg3.Font = new System.Drawing.Font("Microsoft Sans Serif", 22.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.szoveg3.Location = new System.Drawing.Point(12, 116);
+            this.szoveg3.Name = "szoveg3";
+            this.szoveg3.Size = new System.Drawing.Size(228, 44);
+            this.szoveg3.TabIndex = 3;
+            this.szoveg3.Text = "Össz. lövés:";
+            // 
+            // kilep
+            // 
+            this.kilep.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.kilep.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.kilep.Location = new System.Drawing.Point(1091, 30);
+            this.kilep.Name = "kilep";
+            this.kilep.Size = new System.Drawing.Size(144, 46);
+            this.kilep.TabIndex = 4;
+            this.kilep.Text = "Kilépés";
+            this.kilep.UseVisualStyleBackColor = true;
+            this.kilep.Click += new System.EventHandler(this.kilep_Click);
+            // 
+            // ujrakezd
+            // 
+            this.ujrakezd.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.ujrakezd.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.ujrakezd.Location = new System.Drawing.Point(1091, 104);
+            this.ujrakezd.Name = "ujrakezd";
+            this.ujrakezd.Size = new System.Drawing.Size(144, 46);
+            this.ujrakezd.TabIndex = 5;
+            this.ujrakezd.Text = "ÚJRA";
+            this.ujrakezd.UseVisualStyleBackColor = true;
+            this.ujrakezd.Click += new System.EventHandler(this.ujrakezd_Click);
+            // 
+            // idozito
+            // 
+            this.idozito.Enabled = true;
+            this.idozito.Interval = 1000;
+            this.idozito.Tick += new System.EventHandler(this.idozito_Tick);
+            // 
+            // szoveg4
+            // 
+            this.szoveg4.AutoSize = true;
+            this.szoveg4.BackColor = System.Drawing.Color.Transparent;
+            this.szoveg4.Font = new System.Drawing.Font("Microsoft Sans Serif", 28.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.szoveg4.Location = new System.Drawing.Point(231, 160);
+            this.szoveg4.Name = "szoveg4";
+            this.szoveg4.Size = new System.Drawing.Size(0, 55);
+            this.szoveg4.TabIndex = 6;
+            // 
+            // kepernyo
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Control;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.ClientSize = new System.Drawing.Size(1272, 803);
+            this.Controls.Add(this.szoveg4);
+            this.Controls.Add(this.ujrakezd);
+            this.Controls.Add(this.kilep);
+            this.Controls.Add(this.szoveg3);
+            this.Controls.Add(this.szoveg2);
+            this.Controls.Add(this.szoveg1);
+            this.Controls.Add(this.acsu);
+            this.Cursor = System.Windows.Forms.Cursors.NoMove2D;
+            this.MaximizeBox = false;
+            this.MaximumSize = new System.Drawing.Size(1290, 850);
+            this.MinimizeBox = false;
+            this.MinimumSize = new System.Drawing.Size(1290, 850);
+            this.Name = "kepernyo";
+            this.Text = "Kill the faszfej";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.MouseClick += new System.Windows.Forms.MouseEventHandler(this.kepernyo_MouseClick);
+            ((System.ComponentModel.ISupportInitialize)(this.acsu)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox acsu;
+        private System.Windows.Forms.Label szoveg1;
+        private System.Windows.Forms.Label szoveg2;
+        private System.Windows.Forms.Label szoveg3;
+        private System.Windows.Forms.Button kilep;
+        private System.Windows.Forms.Button ujrakezd;
+        private System.Windows.Forms.Timer idozito;
+        private System.Windows.Forms.Label szoveg4;
+    }
+}
+
---
+++ acsu lövős/acsu lövős/Form1.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Media;
+using System.Speech.Synthesis;
+
+namespace acsu_lövős
+{
+    public partial class kepernyo : Form
+    {
+        Random vel = new Random();
+        int pont = 0;
+        int rontott = 0;
+        int osszes = 0;
+
+        public kepernyo()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        void Loves()
+        {
+            pont++;
+            szoveg1.Text = "Pont: " + pont;
+            osszes++;
+            szoveg3.Text = "Össz. lövés: " + osszes;
+
+            Loveshang();
+        }
+        void FelreLoves()
+        {
+            rontott++;
+            szoveg2.Text = "Szar vagy: " + rontott;
+            osszes++;
+            szoveg3.Text = "Össz. lövés: " + osszes;
+
+            Loveshang();
+        }
+
+        void Ujra()
+        {
+            pont = 0;
+            rontott = 0;
+            osszes = 0;
+            szoveg1.Text = "Pont: " + pont;
+            szoveg2.Text = "Szar vagy: " + rontott;
+            szoveg3.Text = "Össz. lövés: " + osszes;
+            szoveg4.Text = "";
+
+            idozito.Start();
+        }
+
+        SpeechSynthesizer beszel = new SpeechSynthesizer();
+        int melyikSzoveg;
+        List<string> lista = new List<string>();
+        void acsuBeszel()
+        {
+            lista.Add("MIT CSINÁLNAK BURAK");
+            lista.Add("A kisnyúl nem bassza meg");
+            lista.Add("Lecsavarom a töküket");
+            lista.Add("Erre sem képesek buraaak");
+            lista.Add("Beszopják a mlretes harcifaszt!");
+            lista.Add("Csak a töküket verik?");
+
+            melyikSzoveg = vel.Next(0, lista.Count());
+
+            beszel.Speak(lista[melyikSzoveg]);
+
+
+        }
+
+        void Loveshang()
+        {
+
+            SoundPlayer hang = new SoundPlayer(@"C:\Users\Andras\Desktop\GAMEK\acsu lövős\hang.wav");
+            hang.Play();
+        }
+
+        private void acsu_Click(object sender, EventArgs e)
+        {
+            Loves();
+            acsuBeszel();
+        }
+
+        private void idozito_Tick(object sender, EventArgs e)
+        {
+            int x;
+            int y;
+            x = vel.Next(50, kepernyo.ActiveForm.Width - 150);
+            y = vel.Next(300, kepernyo.ActiveForm.Height - 150);
+
+            acsu.Location = new Point(x, y);
+
+            if (rontott >= 10)
+            {
+                idozito.Stop();
+                szoveg4.Text = "VESZTETTÉL ACSU JOBB NÁLAD";
+            }
+
+            if (pont >= 10)
+            {
+                idozito.Stop();
+                szoveg4.Text = "LEGYŐZTED A FASZFEJT";
+            }
+
+        }
+
+        private void kepernyo_MouseClick(object sender, MouseEventArgs e)
+        {
+            FelreLoves();
+        }
+
+        private void ujrakezd_Click(object sender, EventArgs e)
+        {
+            Ujra();
+        }
+
+        private void kilep_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+    }
+}
